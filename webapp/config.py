0ab7c9c51e85b41ff095adb5ccc9f24c91ecaa96
Ekimerton <ekim0252@gmail.com>
2019-09-17 22:19:47 -0400

    fixed heroku issue

--- webapp/config.py
+++ webapp/config.py
@@ -5,9 +5,10 @@ class Config:
     # App
     try:
         SECRET_KEY = os.environ['SECRET_KEY']
+        DEBUG = False
     except:
         SECRET_KEY = 'test'
-    	DEBUG = True
+        DEBUG = True
     # Database
     #SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     #SQLALCHEMY_TRACK_MODIFICATIONS = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
